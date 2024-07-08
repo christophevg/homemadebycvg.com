d9025b61c2e00eb66e3e0863e49703034965494d
Christophe VG <contact@christophe.vg>
2024-07-08 16:28:46 +0200

    made folders absolute

--- __init__.py
+++ __init__.py
@@ -4,16 +4,18 @@ from pathlib import Path
 
 import yaml
 
+HERE = Path(__file__).parent
+
 app = Flask(
   "homemadebycvg",
-  template_folder="templates",
-  static_folder="templates/static",
+  template_folder= HERE / "templates",
+  static_folder= HERE / "templates/static",
   static_url_path=""
 )
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-with open(Path(__file__).parent / "makes.yaml") as fp:
+with open(HERE / "makes.yaml") as fp:
   config = yaml.safe_load(fp)
 
 @app.route("/")

d0b7a30e093f5c5c4b8ce7301ad246fd9f0b0245
Christophe VG <contact@christophe.vg>
2024-07-10 18:39:18 +0200

    added apps and generalised buttons

--- __init__.py
+++ __init__.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template
 from pathlib import Path
 
 import yaml
+from collections import namedtuple
+import markdown
 
 HERE = Path(__file__).parent
 
@@ -18,6 +20,20 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 with open(HERE / "makes.yaml") as fp:
   config = yaml.safe_load(fp)
 
+for make in config["makes"].values():
+  make["description"] = markdown.markdown(make.pop("description", ""))
+
+Button = namedtuple("Button", "name title icon")
+
+buttons = [
+  Button("info",      "get more info...",          "info-circle"),
+  Button("run",       "try it...",                 "display"),
+  Button("docs",      "read the docs...",          "book"),
+  Button("pypi",      "see the module on PyPi...", "box-seam"),
+  Button("github",    "see the code on GitHub...", "github" ),
+  Button("instagram", "as seen on Instagram...",   "instagram" )
+]
+
 @app.route("/")
 def home():
-  return render_template("index.html", makes=config["makes"])
+  return render_template("index.html", makes=config["makes"], buttons=buttons)
